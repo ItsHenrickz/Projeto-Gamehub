fba986337a840bd1ca728e6c98f22d6d10f28f6b
ItsHenrickz <canalluiz2029@gmail.com>
2025-11-03 16:02:37 -0300

    Ajuste css

--- admin/clientes-excluir.php
+++ admin/clientes-excluir.php
@@ -7,7 +7,7 @@ $sql = "DELETE FROM clientes WHERE id = '$id'";
 
 if(mysqli_query($conexao, $sql)){
     echo "<br><h2 style='margin:45px 0'>Cliente Excluido com Sucesso.</h2>";
-    echo "<a class='botao' href='?pg=clientes-admin'>Voltar</a>";
+    echo "<a class='botao' style='margin-left: 45%;' href='?pg=clientes-admin'>Voltar</a>";
 }else{
     echo "<br><h2>Erro ao excluir Cliente.</h2>";
     echo "<a href='?pg=clientes-admin'>Voltar aos clientes</a>";
